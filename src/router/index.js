8124e43cecea6e01f6594a535b6d188d09ea71f6
baojf <baojf@fshows.com>
2018-12-18 17:03:03 +0800

    猫崽有限公司-1218

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '../components/hello'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import Index from '../view/index/index'
+import Login from '../view/login'
+import Blog from '../view/blog/index'
+
+Vue.use(Router)
+Vue.use(ElementUI)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'index',
+      component: Index
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/blog',
+      name: 'Blog',
+      component: Blog
+    }
+  ]
+})
